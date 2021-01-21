cb04278d495e2acd41eddd3bb4e85e62d08cb31d
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-01-21 13:35:03 +0000

    repository fixed

--- src/main/java/com/hyusein/mustafa/todoapp/repository/TodoRepository.java
+++ src/main/java/com/hyusein/mustafa/todoapp/repository/TodoRepository.java
@@ -1,7 +1,8 @@
 package com.hyusein.mustafa.todoapp.repository;
 
+import com.hyusein.mustafa.todoapp.model.Todo;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TodoRepository extends CrudRepository {
+public interface TodoRepository extends CrudRepository<Todo, Long> {
 
 }
