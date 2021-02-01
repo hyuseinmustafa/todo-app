33090751c8c7e937d0fa2d9012e585c5583bab3e
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-02-01 14:24:38 +0000

    added Thymeleaf Fragments

--- src/main/java/com/hyusein/mustafa/todoapp/controller/IndexController.java
+++ src/main/java/com/hyusein/mustafa/todoapp/controller/IndexController.java
@@ -1,24 +1,14 @@
 package com.hyusein.mustafa.todoapp.controller;
 
-import com.hyusein.mustafa.todoapp.ToDoStatus;
-import com.hyusein.mustafa.todoapp.command.TodoCommand;
-import com.hyusein.mustafa.todoapp.converter.TodoCommandToTodoConverter;
-import com.hyusein.mustafa.todoapp.converter.TodoToTodoCommandConverter;
-import com.hyusein.mustafa.todoapp.model.Project;
 import com.hyusein.mustafa.todoapp.model.Todo;
-import com.hyusein.mustafa.todoapp.repository.ProjectRepository;
 import com.hyusein.mustafa.todoapp.repository.TodoRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Controller
