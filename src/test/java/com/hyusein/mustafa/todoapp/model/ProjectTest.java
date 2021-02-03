5fd916bfbb73f22efeb224bad2f399bc9ddbcc35
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-02-03 13:24:37 +0000

    Added Test to Models

---
+++ src/test/java/com/hyusein/mustafa/todoapp/model/ProjectTest.java
@@ -0,0 +1,44 @@
+package com.hyusein.mustafa.todoapp.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectTest {
+    private final static Long ID = 1L;
+    private final static String NAME = "name";
+    private final static Todo TODO = Todo.builder().build();
+
+    Project project;
+
+    @BeforeEach
+    void setUp() {
+        project = new Project();
+    }
+
+    @Test
+    void getId() {
+        project.setId(ID);
+
+        assertEquals(project.getId(), ID);
+    }
+
+    @Test
+    void getName() {
+        project.setName(NAME);
+
+        assertTrue(project.getName().equals(NAME));
+    }
+
+    @Test
+    void getTodo() {
+        project.setTodos(new HashSet<>());
+        project.getTodos().add(TODO);
+
+        assertTrue(project.getTodos().contains(TODO));
+        assertEquals(project.getTodos().size(), 1);
+    }
+}
---
+++ src/test/java/com/hyusein/mustafa/todoapp/model/TodoTest.java
@@ -0,0 +1,57 @@
+package com.hyusein.mustafa.todoapp.model;
+
+import com.hyusein.mustafa.todoapp.ToDoStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoTest {
+    private static final Long ID = 1L;
+    private static final String HEADLINE = "headline";
+    private static final String DESCRIPTION = "description";
+    private static final ToDoStatus STATUS = ToDoStatus.WAITING;
+    private static final Project PROJECT = new Project();
+
+    Todo todo;
+
+    @BeforeEach
+    void setUp() {
+        todo = new Todo();
+    }
+
+    @Test
+    void getId() {
+        todo.setId(ID);
+
+        assertEquals(todo.getId(), ID);
+    }
+
+    @Test
+    void getHeadline() {
+        todo.setHeadline(HEADLINE);
+
+        assertTrue(todo.getHeadline().equals(HEADLINE));
+    }
+
+    @Test
+    void getDescription() {
+        todo.setDescription(DESCRIPTION);
+
+        assertTrue(todo.getDescription().equals(DESCRIPTION));
+    }
+
+    @Test
+    void getStatus() {
+        todo.setStatus(STATUS);
+
+        assertTrue(todo.getStatus().equals(STATUS));
+    }
+
+    @Test
+    void getProject() {
+        todo.setProject(PROJECT);
+
+        assertEquals(todo.getProject(), PROJECT);
+    }
+}
