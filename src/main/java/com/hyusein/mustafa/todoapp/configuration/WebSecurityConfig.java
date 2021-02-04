e4031d5815f6646d10e9befc3e36bcef3d518e6d
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-02-04 16:31:23 +0000

    disabled multi login, not working

--- src/main/java/com/hyusein/mustafa/todoapp/configuration/WebSecurityConfig.java
+++ src/main/java/com/hyusein/mustafa/todoapp/configuration/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.hyusein.mustafa.todoapp.configuration;
 
 import com.hyusein.mustafa.todoapp.service.UserDetailsServiceImpl;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
@@ -39,24 +41,46 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(authenticationProvider());
     }
 
+    // Register HttpSessionEventPublisher
+    @Bean
+    public static ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/registration").permitAll()
-                    .antMatchers("/h2-console/**").permitAll()//added for h2-console access without authentication
-                    .anyRequest().authenticated()
-                    .and()
+                    .antMatchers("/registration")
+                        .permitAll()
+                    .antMatchers("/h2-console/**")
+                        .permitAll()
+                //added for h2-console access without authentication
+                    .anyRequest()
+                        .authenticated()
+        .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
+                    .loginPage("/login").permitAll()
+        .and()
                 .logout()
                     .deleteCookies("JSESSIONID")
                     .permitAll()
-                .and().csrf().ignoringAntMatchers("/h2-console/**")//added for h2-console access without authentication
-                .and().headers().frameOptions().sameOrigin()//added for h2-console access without authentication
-                .and().rememberMe().key("uniqueAndSecret")
+        .and()
+                .csrf()
+                    .ignoringAntMatchers("/h2-console/**")
+                //added for h2-console access without authentication
+        .and()
+                .headers()
+                    .frameOptions()
+                        .sameOrigin()
+                //added for h2-console access without authentication
+        .and()
+                .rememberMe()
+                    .key("rememberMeToken")
+        .and()
+                .sessionManagement()
+                    .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
         ;
     }
 }
