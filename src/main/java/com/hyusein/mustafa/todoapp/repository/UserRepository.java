8f661a6781a38c7434317b76b37cf4a368d185b3
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-02-04 13:20:43 +0000

    Added findByEmail

--- src/main/java/com/hyusein/mustafa/todoapp/repository/UserRepository.java
+++ src/main/java/com/hyusein/mustafa/todoapp/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.hyusein.mustafa.todoapp.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    User getUserByUsername(String username);
+    User findByUsername(String username);
+    User findByEmail(String email);
 }
