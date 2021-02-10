d4720f3373b54d3c73fb4a965cf623b727dc7e96
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-02-10 08:26:48 +0000

    Annotation changed

--- src/main/java/com/hyusein/mustafa/todoapp/command/ProjectCommand.java
+++ src/main/java/com/hyusein/mustafa/todoapp/command/ProjectCommand.java
@@ -5,15 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Setter
 @Getter
 @NoArgsConstructor
 public class ProjectCommand {
+
     private Long id;
-    @NotNull
+
+    @NotBlank
     @Size(min = 3, max = 20)
     private String name;
 
