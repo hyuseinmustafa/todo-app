68bdffc8a755c0bb7013c39d378409cf5f844805
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-02-17 10:42:44 +0000

    bug fix ifIdPresentRemediate was returning null if there the id property is null

--- src/main/java/com/hyusein/mustafa/todoapp/model/Todo.java
+++ src/main/java/com/hyusein/mustafa/todoapp/model/Todo.java
@@ -6,6 +6,7 @@ import com.hyusein.mustafa.todoapp.tool.Tool;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.function.Function;
 
 @Getter
@@ -19,8 +20,10 @@ public class Todo extends Auditable<String> implements Tool{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String headline;
+
     @Lob
     private String description;
+
     @Enumerated(EnumType.STRING)
     private ToDoStatus status;
 
@@ -33,18 +36,21 @@ public class Todo extends Auditable<String> implements Tool{
     @JoinColumn(name = "assigned_user_id")
     private User assignedUser;
 
+    private Date deadline;
+
     @Builder
-    public Todo(Long id, String headline, String description, ToDoStatus status, Project project, User assignedUser) {
+    public Todo(Long id, String headline, String description, ToDoStatus status, Project project, User assignedUser, Date deadline) {
         this.id = id;
         this.headline = headline;
         this.description = description;
         this.status = status;
         this.project = project;
         this.assignedUser = assignedUser;
+        this.deadline = deadline;
     }
 
     public Todo ifIdPresentRemediate(Function<Todo, Todo> source){
-        if(this.id == null) return null;
+        if(this.id == null) return this;
         return Tool.remediate(this, source.apply(this));
     }
 }
