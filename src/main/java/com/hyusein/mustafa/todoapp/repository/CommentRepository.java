634f0baea1f746ba0e5567a7dbf3090d11ec1f8f
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-03-02 14:25:28 +0000

    added find by todo id to Comment Repository

--- src/main/java/com/hyusein/mustafa/todoapp/repository/CommentRepository.java
+++ src/main/java/com/hyusein/mustafa/todoapp/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.hyusein.mustafa.todoapp.repository;
 
 import com.hyusein.mustafa.todoapp.model.Comment;
+import com.hyusein.mustafa.todoapp.model.Todo;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+    Set<Comment> findByTodo(Todo todo);
 }
