364df01e54f285864be6c1d8f46d90cbad479068
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-03-02 12:34:38 +0000

    Added comment model and modified related models

--- src/main/java/com/hyusein/mustafa/todoapp/model/Todo.java
+++ src/main/java/com/hyusein/mustafa/todoapp/model/Todo.java
@@ -1,5 +1,6 @@
 package com.hyusein.mustafa.todoapp.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.hyusein.mustafa.todoapp.ToDoStatus;
 import com.hyusein.mustafa.todoapp.tool.Tool;
@@ -7,6 +8,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Set;
 import java.util.function.Function;
 
 @Getter
@@ -42,8 +44,13 @@ public class Todo extends Auditable<String> implements Tool{
 
     private Date deadline;
 
+    @JsonBackReference
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "todo")
+    private Set<Comment> comments;
+
     @Builder
-    public Todo(Long id, String headline, String description, ToDoStatus status, Project project, User assignedUser, Date deadline) {
+    public Todo(Long id, String headline, String description, ToDoStatus status, Project project,
+                User assignedUser, Date deadline, Set<Comment> comments) {
         this.id = id;
         this.headline = headline;
         this.description = description;
@@ -51,6 +58,7 @@ public class Todo extends Auditable<String> implements Tool{
         this.project = project;
         this.assignedUser = assignedUser;
         this.deadline = deadline;
+        this.comments = comments;
     }
 
     public Todo ifIdPresentRemediate(Function<Todo, Todo> source){
