677306c2147c3503403a64cf699dd561c1397c52
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-03-03 21:57:28 +0000

    added filter index poage to filter list

--- src/main/java/com/hyusein/mustafa/todoapp/controller/IndexController.java
+++ src/main/java/com/hyusein/mustafa/todoapp/controller/IndexController.java
@@ -1,10 +1,21 @@
 package com.hyusein.mustafa.todoapp.controller;
 
+import com.hyusein.mustafa.todoapp.enums.Priority;
+import com.hyusein.mustafa.todoapp.enums.ToDoStatus;
+import com.hyusein.mustafa.todoapp.model.Todo;
+import com.hyusein.mustafa.todoapp.model.User;
 import com.hyusein.mustafa.todoapp.service.TodoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -18,10 +29,21 @@ public class IndexController {
     }
 
     @GetMapping({"","/"})
-    public String getIndexPage(Model model) {
+    public String getIndexPage(@ModelAttribute("project") String projectName,
+                               @ModelAttribute("priority") String priority,
+                               @ModelAttribute("status") String status,
+                               @ModelAttribute("username") String username,
+                               Model model) {
         log.debug("Index Page Requested.");
 
-        model.addAttribute("todo_list", todoService.findAll());
+        model.addAttribute("todo_list", todoService.findAll().stream()
+                .filter(todo -> projectName.isEmpty() || projectName.toLowerCase().equals(
+                        todo.getProject().getName().toLowerCase()))
+                .filter(todo -> priority.isEmpty() || priority.toUpperCase().equals(todo.getPriority().name()))
+                .filter(todo -> status.isEmpty() || status.toUpperCase().equals(todo.getStatus().name()))
+                .filter(todo -> username.isEmpty() || username.toLowerCase().equals(
+                        Optional.ofNullable(todo.getAssignedUser()).orElse(User.builder().username("").build()).getUsername().toLowerCase()))
+                .collect(Collectors.toList()));
 
         return "index";
     }
